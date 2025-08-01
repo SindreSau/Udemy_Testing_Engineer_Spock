15b831347168d8c5559b6514ff78b6a6d54c58ee
SindreSau <sindre_50@hotmail.com>
2025-08-01 17:04:39 +0200

    TicTacToe-tests using TDD principles

--- Business.Tests/TicTacToeTests.cs
+++ Business.Tests/TicTacToeTests.cs
@@ -3,5 +3,87 @@ namespace Business.Tests;
 [TestFixture]
 public class TicTacToeTests
 {
-    
+    [Test]
+    public void CreateGame_ZeroMoves()
+    {
+        var game = new TicTacToeGame();
+        Assert.That(game.MovesCounter, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void MakeMove_IncrementsMovesCounter()
+    {
+        var game = new TicTacToeGame();
+        game.MakeMove(1);
+        Assert.That(game.MovesCounter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void MakeInvalidMove_ThrowsException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var game = new TicTacToeGame();
+            game.MakeMove(0);
+        });
+    }
+
+    [Test]
+    public void MoveOnTheSameSquare_ThrowsException()
+    {
+        var game = new TicTacToeGame();
+        game.MakeMove(1);
+        Assert.Throws<InvalidOperationException>(() => game.MakeMove(1));
+    }
+
+    [Test]
+    public void MakingMoves_SetStateCorrectly()
+    {
+        var game = new TicTacToeGame();
+        game.MakeMove(1);
+        game.MakeMove(2);
+        game.MakeMove(3);
+        game.MakeMove(4);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(game.MovesCounter, Is.EqualTo(4));
+            Assert.That(game.GetState(1), Is.EqualTo(State.Zero));
+            Assert.That(game.GetState(2), Is.EqualTo(State.Cross));
+            Assert.That(game.GetState(3), Is.EqualTo(State.Zero));
+            Assert.That(game.GetState(4), Is.EqualTo(State.Cross));
+        });
+    }
+
+    [Test]
+    public void InitialBoardState_AllUnset()
+    {
+        var game = new TicTacToeGame();
+        Assert.Multiple(() =>
+        {
+            for (var i = 1; i <= 9; i++)
+                Assert.That(
+                    game.GetState(i),
+                    Is.EqualTo(State.Unset)
+                );
+        });
+    }
+
+    [Test]
+    public void GetWinner_ZeroWinVertically_ReturnsZeroes()
+    {
+        var game = new TicTacToeGame();
+        game.MakeMoves(1, 2, 4, 5, 7, 8);
+
+        Assert.That(game.GetWinner(), Is.EqualTo(Winner.Zero));
+    }
+
+    [Test]
+    public void GetWinner_CrossWinHorizontally_ReturnsCrosses()
+    {
+        var game = new TicTacToeGame();
+        game.MakeMoves(4, 1, 5, 2, 7, 3);
+
+        Assert.That(game.GetWinner(), Is.EqualTo(Winner.Cross));
+    }
 }
--- Business/TicTacToeGame.cs
+++ Business/TicTacToeGame.cs
@@ -1,6 +1,110 @@
+using System.Text;
+
 namespace Business;
+
+public enum State
+{
+    Cross,
+    Zero,
+    Unset
+}
+
+public enum Winner
+{
+    Cross,
+    Zero,
+    None
+}
 
 public class TicTacToeGame
 {
-    
+    private readonly State[] _board = new State[9];
+
+    public TicTacToeGame()
+    {
+        for (var i = 0; i < _board.Length; i++) _board[i] = State.Unset;
+    }
+
+    public int MovesCounter { get; private set; }
+
+
+    public void MakeMove(int i)
+    {
+        if (i is < 1 or > 9)
+            throw new ArgumentOutOfRangeException(nameof(i), "Move must be between 1 and 9 inclusive.");
+
+        if (GetState(i) != State.Unset)
+            throw new InvalidOperationException($"Square {i} is already occupied.");
+
+        _board[i - 1] = MovesCounter % 2 == 0 ? State.Zero : State.Cross;
+        MovesCounter++;
+    }
+
+    public void MakeMoves(params int[] moves)
+    {
+        foreach (var move in moves) MakeMove(move);
+    }
+
+    public Winner GetWinner()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            if (_board[i * 3] != State.Unset &&
+                _board[i * 3] == _board[i * 3 + 1] &&
+                _board[i * 3] == _board[i * 3 + 2])
+                return _board[i * 3] == State.Cross ? Winner.Cross : Winner.Zero;
+
+            if (_board[i] != State.Unset &&
+                _board[i] == _board[i + 3] &&
+                _board[i] == _board[i + 6])
+                return _board[i] == State.Cross ? Winner.Cross : Winner.Zero;
+        }
+
+        if (_board[0] != State.Unset &&
+            _board[0] == _board[4] &&
+            _board[0] == _board[8])
+            return _board[0] == State.Cross ? Winner.Cross : Winner.Zero;
+
+        if (_board[2] != State.Unset &&
+            _board[2] == _board[4] &&
+            _board[2] == _board[6])
+            return _board[2] == State.Cross ? Winner.Cross : Winner.Zero;
+
+        return Winner.None;
+    }
+
+    public State GetState(int i)
+    {
+        return _board[i - 1];
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        for (var i = 1; i <= 9; i++)
+        {
+            var state = GetState(i);
+            sb.Append(' ');
+            sb.Append(state switch
+            {
+                State.Cross => 'X',
+                State.Zero => 'O',
+                _ => ' '
+            });
+            sb.Append(' ');
+
+            if (i % 3 == 0)
+            {
+                if (i >= 9) continue;
+                sb.AppendLine();
+                sb.AppendLine("---+---+---");
+            }
+            else
+            {
+                sb.Append('|');
+            }
+        }
+
+        return sb.ToString();
+    }
 }
